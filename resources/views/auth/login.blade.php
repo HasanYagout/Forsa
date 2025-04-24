1fa2bf6457716be3622b946a30574bca13c61384
hasan yagout <yagouthasan3@gmail.com>
2025-04-24 13:02:11 +0300

    fix stuff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
     <div class="min-h-screen flex flex-col items-center justify-center bg-[#EAEFF8] py-12 px-4">
         <div class="text-center mb-6">
-            <h1 class="text-2xl font-bold">CHANCES</h1>
-            <p class="text-sm text-gray-500">Your Next Chance Starts Here</p>
+            <h1 class="text-2xl font-bold">{{__('CHANCES')}}</h1>
+            <p class="text-sm text-gray-500">{{__('Your Next Chance Starts Here')}}</p>
         </div>
 
         <div class="w-full max-w-md bg-white rounded-xl shadow-md p-10  transition-all">
             <!-- Tabs -->
             <div class="flex justify-center mb-8 space-x-6">
-                <button id="tab-login-btn" class="pb-1 cursor-pointer text-lg font-medium text-indigo-600 border-b-2 border-indigo-500">Sign</button>
-                <button id="tab-register-btn" class="pb-1 cursor-pointer text-lg font-medium text-gray-500">Register</button>
+                <button id="tab-login-btn" class="pb-1 cursor-pointer text-lg font-medium text-indigo-600 border-b-2 border-indigo-500">{{__('Sign')}}</button>
+                <button id="tab-register-btn" class="pb-1 cursor-pointer text-lg font-medium text-gray-500">{{__('Register')}}</button>
             </div>
 
             <!-- Login Form -->
@@ -26,30 +26,30 @@
                     @csrf
 
                     <div class="mb-4">
-                        <x-label for="email" value="Email Address" />
-                        <x-input id="email" class="block mt-1 p-2 w-full border rounded-md" type="email" name="email" :value="old('email')" placeholder="Email Address" required autofocus />
+                        <x-label for="email" value="{{__('Email Address')}}" />
+                        <x-input id="email" class="block mt-1 p-2 w-full border rounded-md" type="email" name="email" :value="old('email')" placeholder="{{__('Email Address')}}" required autofocus />
                     </div>
 
                     <div class="mb-4">
-                        <x-label for="password" value="Password" />
-                        <x-input id="password" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password" placeholder="Password" required />
+                        <x-label for="password" value="{{__('Password')}}" />
+                        <x-input id="password" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password" placeholder="{{__('Password')}}" required />
                     </div>
 
                     <div class="flex items-center justify-between mb-6">
                         <label for="remember_me" class="flex items-center">
                             <x-checkbox id="remember_me" name="remember" />
-                            <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                            <span class="ms-2 text-sm text-gray-600">{{__('Remember me')}}</span>
                         </label>
 
                         @if (Route::has('password.request'))
                             <a class="text-sm text-indigo-600 hover:underline" href="{{ route('password.request') }}">
-                                Forgot password?
+                                {{__('Forgot password?')}}
                             </a>
                         @endif
                     </div>
 
                     <x-button class="w-full justify-center  hover:bg-indigo-600 text-white py-2 rounded-md">
-                        Submit
+                        {{__('Submit')}}
                     </x-button>
                 </form>
             </div>
@@ -58,33 +58,32 @@
             <div id="tab-register" class="hidden">
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
-
                     <div class="mb-4">
-                        <x-label for="first_name" value="First Name" />
-                        <x-input id="first_name" class="block mt-1 p-2 w-full border rounded-md" type="text" name="first_name" placeholder="First Name" required />
+                        <x-label for="first_name" value="{{__('First Name')}}" />
+                        <x-input id="first_name" class="block mt-1 p-2 w-full border rounded-md" type="text" name="first_name" placeholder="{{__('First Name')}}" required />
                     </div>
                     <div class="mb-4">
-                        <x-label for="last_name" value="Last Name" />
-                        <x-input id="last_name" class="block mt-1 p-2 w-full border rounded-md" type="text" name="last_name" placeholder="Last Name" required />
-                    </div>
-
-                    <div class="mb-4">
-                        <x-label for="email" value="Email Address" />
-                        <x-input id="email" class="block mt-1 p-2 w-full border rounded-md" type="email" name="email" placeholder="Email Address" required />
+                        <x-label for="last_name" value="{{__('Last Name')}}" />
+                        <x-input id="last_name" class="block mt-1 p-2 w-full border rounded-md" type="text" name="last_name" placeholder="{{__('Last Name')}}" required />
                     </div>
 
                     <div class="mb-4">
-                        <x-label for="password" value="Password" />
-                        <x-input id="password" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password" placeholder="Password" required />
+                        <x-label for="email" value="{{__('Email Address')}}" />
+                        <x-input id="email" class="block mt-1 p-2 w-full border rounded-md" type="email" name="email" placeholder="{{__('Email Address')}}" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <x-label for="password" value="{{__('Password')}}" />
+                        <x-input id="password" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password" placeholder="{{__('Password')}}" required />
                     </div>
 
                     <div class="mb-6">
-                        <x-label for="password_confirmation" value="Confirm Password" />
-                        <x-input id="password_confirmation" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password_confirmation" placeholder="Confirm Password" required />
+                        <x-label for="password_confirmation" value="{{__('Confirm Password')}}" />
+                        <x-input id="password_confirmation" class="block mt-1 p-2 w-full border rounded-md" type="password" name="password_confirmation" placeholder="{{__('Confirm Password')}}" required />
                     </div>
 
                     <x-button class="w-full justify-center bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded-md">
-                        Register
+                        {{__('Register')}}
                     </x-button>
                 </form>
             </div>
