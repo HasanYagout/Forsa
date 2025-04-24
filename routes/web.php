4ee254db32263a9403e55fe09c4c9dac82294107
hasan yagout <yagouthasan3@gmail.com>
2025-04-24 12:12:54 +0300

    fix stuff

--- routes/web.php
+++ routes/web.php
@@ -5,37 +5,38 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('dashboard')->middleware(\App\Http\Middleware\SetLocale::class);
-Route::get('/about_us', [HomeController::class, 'about_us'])->name('about_us')->middleware(\App\Http\Middleware\SetLocale::class);;
-Route::get('/contact_us', [\App\Http\Controllers\ContactController::class, 'index'])->name('contact.index')->middleware(\App\Http\Middleware\SetLocale::class);;
-Route::post('/contact', [\App\Http\Controllers\ContactController::class, 'store'])->name('contact.store')->middleware(\App\Http\Middleware\SetLocale::class);;
+// Group all routes that require the SetLocale middleware
+Route::middleware([\App\Http\Middleware\SetLocale::class])->group(function () {
+    // Public Routes
+    Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+    Route::get('/about_us', [HomeController::class, 'about_us'])->name('about_us');
+    Route::get('/contact_us', [\App\Http\Controllers\ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [\App\Http\Controllers\ContactController::class, 'store'])->name('contact.store');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-});
+    // Bookmarks Routes
+    Route::post('/bookmarks', [BookmarkController::class, 'toggle'])
+        ->middleware('auth')
+        ->name('bookmarks.toggle');
+    Route::get('/bookmarks/show', [BookmarkController::class, 'show'])
+        ->name('bookmark.show');
 
-Route::post('/bookmarks', [BookmarkController::class, 'toggle'])
-    ->middleware('auth')
-    ->name('bookmarks.toggle');
-Route::get('/bookmarks/show', [BookmarkController::class, 'show'])
-    ->name('bookmark.show');
-    Route::get('/dashboard', [HomeController::class,'index'])->name('dashboard');
-Route::group(['middleware' => ['web'],'prefix'=>'jobs','as'=>'jobs.'], function () {
-    Route::get('/', [JobController::class, 'index'])->name('index');
-    Route::get('/view/{slug}', [JobController::class, 'view'])->name('view');
-});
+    // Jobs Routes
+    Route::group(['middleware' => ['web'], 'prefix' => 'jobs', 'as' => 'jobs.'], function () {
+        Route::get('/', [JobController::class, 'index'])->name('index');
+        Route::get('/view/{slug}', [JobController::class, 'view'])->name('view');
+    });
 
-//Route::group(['middleware' => ['web'],'prefix'=>'tenders','as'=>'tenders.'], function () {
+    // Trainings Routes
+    Route::group(['middleware' => ['web'], 'prefix' => 'trainings', 'as' => 'trainings.'], function () {
+        Route::get('/', [\App\Http\Controllers\TrainingController::class, 'index'])->name('index');
+        Route::get('/view/{slug}', [\App\Http\Controllers\TrainingController::class, 'view'])->name('view');
+    });
+
+    //Route::group(['middleware' => ['web'],'prefix'=>'tenders','as'=>'tenders.'], function () {
 //    Route::get('/', [\App\Http\Controllers\TenderController::class, 'index'])->name('index');
 //    Route::get('/view/{slug}', [\App\Http\Controllers\TenderController::class, 'view'])->name('view');
 //});
-
-Route::group(['middleware' => ['web'],'prefix'=>'trainings','as'=>'trainings.'], function () {
-    Route::get('/', [\App\Http\Controllers\TrainingController::class, 'index'])->name('index')->middleware(\App\Http\Middleware\SetLocale::class);;
-    Route::get('/view/{slug}', [\App\Http\Controllers\TrainingController::class, 'view'])->name('view');
 });
 
-Route::get('language/{locale}', [App\Http\Controllers\HomeController::class, 'switchLanguage'])->name('language.switch');
+// Language Switch Route
+Route::get('language/{locale}', [HomeController::class, 'switchLanguage'])->name('language.switch');
